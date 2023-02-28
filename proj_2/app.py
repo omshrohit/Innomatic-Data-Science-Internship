3d5b4c547e43275709abd555263d3524e3136aa1
omshrohit <114162450+omshrohit@users.noreply.github.com>
2023-02-28 13:49:09 +0530

    Add files via upload

---
+++ proj_2/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title(":red[Innomatics Data App]")
+st.header("Hii I am Creatng My First Streanlit App ")
+st.subheader(":blue[With the Help Of KANAV Sir]")
+st.subheader("This is MY First Assignment Of Data Science")
+st.snow()
+
+btn_click = st.button("Click Here to Know About Rohit Yadav")
+
+if btn_click == True:
+    st.subheader("I am Rohit Yadav")
+    st.subheader("I am Studying In JCT College Of Engineering And Technology, Combatore,Tamilnadu")
+    st.subheader(":red[Let's Strat Baby]")
+    st.balloons()
